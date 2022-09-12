e8a740e3802e138b3d1999c3f642e09e9757e955
Иван <ivankiseliov@rambler.ru>
2022-09-12 22:26:23 +0300

    задание 6

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        //задание 5
-        int age = 33;
-        if ( age >= 24 ) {
-            System.out.println("Человек уже закончил школу и может отправляться в университет");
-        } else if ( age >= 18 ) {
-            System.out.println("Человек окончил университет и ему пора искать первую работу");
-        } else if ( age >7 ){
-            System.out.println("Ребенок ходит в школу");
+        //задание 6
+        int all = 102;
+        int sit = 60;
+        int mesto = 33; //текущее место
+        if (mesto <= sit) {
+            System.out.println("Ваше место сидячее");
+        } else if (mesto <= all) {
+            System.out.println("Ваше место стоячее");
+        } else if (mesto > all) {
+            System.out.println("Мест нет, вагон полный");
         }
     }
 }
