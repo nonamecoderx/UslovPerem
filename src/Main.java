8bf932e91c2fcb1c11216049ed8694a25e77e923
Иван <ivankiseliov@rambler.ru>
2022-09-12 21:37:57 +0300

    задание 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //задание 1
+        int age = 16;
+        if ( age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println("Возраст совершеннолетия еще не наступил, необходимо подождать.");
+        }
+    }
+}
