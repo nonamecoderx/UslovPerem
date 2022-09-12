32b25bac76a03d29b6750aef7421af6d8e38c495
Иван <ivankiseliov@rambler.ru>
2022-09-12 22:57:26 +0300

    задание 7

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        //задание 6
-        int all = 102;
-        int sit = 60;
-        int mesto = 33; //текущее место
-        if (mesto <= sit) {
-            System.out.println("Ваше место сидячее");
-        } else if (mesto <= all) {
-            System.out.println("Ваше место стоячее");
-        } else if (mesto > all) {
-            System.out.println("Мест нет, вагон полный");
+        //задание 7
+        int age = 99;
+        boolean goToGarden = age >= 2 && age <=6;
+        if (goToGarden) {
+            System.out.println("Если возраст равен " + age + " , то нужно ходить в детский сад");
+        }
+        boolean goToSchool = age >=7 && age <= 18;
+        if (goToSchool) {
+            System.out.println("Если возраст равен " + age + " , то нужно ходить в школу");
+        }
+        boolean goToUniverse = age >18 && age < 24;
+        if (goToUniverse) {
+            System.out.println("Если возраст равен " + age + " , то нужно ходить в университет");
+        }
+        boolean goToWork = age >=24;
+        if (goToWork) {
+            System.out.println("Если возраст равен " + age + " , то нужно ходить на работу");
         }
     }
 }
